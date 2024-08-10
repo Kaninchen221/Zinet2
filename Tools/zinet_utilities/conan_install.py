1fef795b662444ee6e79b3e705d60abac530b26b
Kaninchen221 <pawel8krolik@gmail.com>
2024-08-10 13:26:29 +0200

    ZIN-96
    - Modify conan_install so it can handle differents conan profiles
    - 4 profiles for Windows/Linux, MSVC/GCC, Debug/Release
    - Add options in menu.py

--- Tools/zinet_utilities/conan_install.py
+++ Tools/zinet_utilities/conan_install.py
@@ -1,52 +1,71 @@
 import argparse
-import os
 import subprocess
 import sys
 
-from zinet_utilities.paths import find_zinet_build_path, find_zinet_root_path, find_conan_profiles_folder, \
-    get_build_folder_name
+from paths import find_zinet_build_path, find_zinet_root_path, find_conan_profiles_folder
 
-parser = argparse.ArgumentParser(description='CMake Build')
 
-parser.add_argument('--BuildType', type=str, help='Debug or Release', default='Debug')
+def validate_arg(arg_name, arg, possible_values):
+    print(f"Arg: {arg_name} has value: {arg}")
+    if not arg.lower() in possible_values:
+        raise Exception(f"Argument: {arg_name} has invalid value: {arg}, possible values: {possible_values}")
 
-args = parser.parse_args()
 
-print('BuildType:', args.BuildType)
+def return_validated_args(parser):
+    parser.add_argument('--BuildType', type=str, help='Debug or Release', default='Debug')
+    parser.add_argument('--Compiler', type=str, help='MSVC, Clang or GCC', default='MSVC')
+    parser.add_argument('--Platform', type=str, help='Windows or Linux', default='Windows')
 
-if args.BuildType != "Debug" and args.BuildType != "Release":
-    raise Exception("Invalid --BuildType")
+    args = parser.parse_args()
 
-if args.BuildType == "Debug":
-    profile_file_name = "conan_profile_windows_vs_debug.txt"
-else:
-    profile_file_name = "conan_profile_windows_vs_release.txt"
+    validate_arg("BuildType", args.BuildType, ["debug", "release"])
+    validate_arg("Compiler", args.Compiler, ["msvc", "clang", "gcc"])
+    validate_arg("Platform", args.Platform, ["windows", "linux"])
 
-profile_path = find_conan_profiles_folder() / profile_file_name
-print(f"Profile path: {profile_path}")
+    return args
 
-zinet_root_path = find_zinet_root_path()
-print(f"Zinet root path: {zinet_root_path}")
 
-build_folder_path = find_zinet_build_path()
-print(f"Zinet build folder path: {build_folder_path}")
+def conan_install():
+    parser = argparse.ArgumentParser(description='Conan install')
+    args = return_validated_args(parser)
 
-subprocess.run("conan remove --locks", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-               universal_newlines=True)
+    build_type = args.BuildType.lower()
+    compiler = args.Compiler.lower()
+    platform = args.Platform.lower()
 
-arguments = (rf'install {zinet_root_path} '
-             f'-pr {profile_path} '
-             f'-pr:b {profile_path} '
-             f'--build="missing" '
-             f'-if {build_folder_path} '
-             f'-of {zinet_root_path} ')
-print(f"Arguments: {arguments}")
+    profile_file_name = f"conan_profile_{platform}_{compiler}_{build_type}.txt"
 
-process = subprocess.run("conan " + arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-                         universal_newlines=True)
-print(process.stdout)
-print(process.stderr)
+    profile_path = find_conan_profiles_folder() / profile_file_name
+    print(f"Profile path: {profile_path}")
+    if not profile_path.exists():
+        raise Exception("Profile path doesn't exist")
 
-if process.returncode != 0:
-    print(f"Conan return code {process.returncode}")
-    sys.exit(1)
+    zinet_root_path = find_zinet_root_path()
+    print(f"Zinet root path: {zinet_root_path}")
+
+    build_folder_path = find_zinet_build_path()
+    print(f"Zinet build folder path: {build_folder_path}")
+
+    subprocess.run("conan remove --locks", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                   universal_newlines=True)
+
+    arguments = (rf'install {zinet_root_path} '
+                 f'-pr {profile_path} '
+                 f'-pr:b {profile_path} '
+                 f'--build="missing" '
+                 f'-if {build_folder_path} '
+                 f'-of {zinet_root_path} ')
+    print(f"Arguments: {arguments}")
+
+    process = subprocess.run("conan " + arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                             universal_newlines=True)
+    print(process.stdout)
+    print(process.stderr)
+
+    if process.returncode != 0:
+        print(f"Conan install return code {process.returncode}")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    conan_install()
