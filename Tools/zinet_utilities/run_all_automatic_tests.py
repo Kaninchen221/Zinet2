da9bd381c2fe53ef18a75c65b8a01b1fd7a7c838
Kaninchen221 <pawel8krolik@gmail.com>
2024-08-29 00:26:30 +0200

    ZIN-96
    - Add compatibility with Linux OS.

--- Tools/zinet_utilities/run_all_automatic_tests.py
+++ Tools/zinet_utilities/run_all_automatic_tests.py
@@ -4,17 +4,27 @@ import sys
 
 from zinet_utilities.paths import find_zinet_bin_path
 
+from zinet_utilities.platform_info import get_system, SystemInfo
+
 bin_folder_path = find_zinet_bin_path()
 print(f"Bin folder path: {bin_folder_path}")
 os.chdir(bin_folder_path)
 
 for file in os.listdir():
-    if file.endswith(".exe") and file.find("Automatic") != -1:
-        process = subprocess.run(file, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-                                 universal_newlines=True)
-        print(process.stdout)
-        print(process.stderr)
+    if get_system() == SystemInfo.Windows and (not file.endswith(".exe") or file.find("Automatic") == -1):
+        continue
 
-        if process.returncode != 0:
-            print(f"Process {file} return code {process.returncode}")
-            sys.exit(1)
+    if get_system() == SystemInfo.Linux and file.find("Automatic") == -1:
+        continue
+
+    if get_system() == SystemInfo.Other:
+        raise Exception("Not supported OS Platform")
+
+    process = subprocess.run(file, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                             universal_newlines=True)
+    print(process.stdout)
+    print(process.stderr)
+
+    if process.returncode != 0:
+        print(f"Process {file} return code {process.returncode}")
+        sys.exit(1)
