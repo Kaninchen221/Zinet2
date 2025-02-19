e116935d85dad9b343f92ab6177ee338f9533e61
Kaninchen221 <pawel8krolik@gmail.com>
2025-02-19 18:00:21 +0100

    Use absolute in CurrentProjectRootPath()

--- Libs/CoreLib/include/Zinet/Core/ZtPaths.hpp
+++ Libs/CoreLib/include/Zinet/Core/ZtPaths.hpp
@@ -12,7 +12,7 @@ namespace zt::core
 		ZINET_CORE_API std::filesystem::path RootPath();
 
 		// It must be inline function
-		inline std::filesystem::path CurrentProjectRootPath() { return ZINET_CURRENT_PROJECT_ROOT_PATH; }
+		inline std::filesystem::path CurrentProjectRootPath() { return std::filesystem::absolute(ZINET_CURRENT_PROJECT_ROOT_PATH); }
 	};
 
 }
