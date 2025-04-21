5bf26bd955b5d0bef32c75bebe5989f4038b3224
Kaninchen221 <pawel8krolik@gmail.com>
2025-04-21 20:09:34 +0200

    ZIN-106 Set requested api version to 1.2

--- Libs/VulkanRendererLib/source/ZtInstance.cpp
+++ Libs/VulkanRendererLib/source/ZtInstance.cpp
@@ -23,10 +23,10 @@ namespace zt::vulkan_renderer
 		VkApplicationInfo appInfo{};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "Zinet Vulkan Renderer";
-		appInfo.applicationVersion = VK_MAKE_VERSION(1, 2, 0);
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "Zinet";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+		appInfo.apiVersion = VK_API_VERSION_1_2;
 
 		VkInstanceCreateInfo createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
