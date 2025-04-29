e2fddcb34e99e6ecc940a6e1af4835f41ff8197d
Kaninchen221 <pawel8krolik@gmail.com>
2025-04-29 21:09:01 +0200

    ZIN-106 Fix linker problem for GNU

--- Libs/VulkanRendererLib/include/Zinet/VulkanRenderer/ZtCommandBuffer.hpp
+++ Libs/VulkanRendererLib/include/Zinet/VulkanRenderer/ZtCommandBuffer.hpp
@@ -40,25 +40,93 @@ namespace zt::vulkan_renderer
 
 		bool create(const Device& device, const CommandPool& commandPool) noexcept;
 
-		inline bool reset() noexcept;
+		bool reset() noexcept;
 
-		inline bool begin() noexcept;
-
-		inline bool end() noexcept;
-
-		inline void beginRenderPass(const RenderPass& renderPass, const Framebuffer& framebuffer, const VkExtent2D& extent, const Vector2i& offset = { 0, 0 }, const VkClearValue& clearValue = DefaultClearColor) noexcept;
-
-		inline void endRenderPass() noexcept;
-
-		inline void bindPipeline(const Pipeline& pipeline) noexcept;
-
-		inline void setViewport(const VkViewport& viewport) noexcept;
-
-		inline void setScissor(const VkRect2D& scissor) noexcept;
-
-		inline void draw(std::uint32_t vertexCount,
-						 std::uint32_t instanceCount,
-						 std::uint32_t firstVertex,
-						 std::uint32_t firstInstance) noexcept;
+		bool begin() noexcept;
+		
+		bool end() noexcept;
+		
+		void beginRenderPass(const RenderPass& renderPass, const Framebuffer& framebuffer, const VkExtent2D& extent, const Vector2i& offset = { 0, 0 }, const VkClearValue& clearValue = DefaultClearColor) noexcept;
+		
+		void endRenderPass() noexcept;
+		
+		void bindPipeline(const Pipeline& pipeline) noexcept;
+		
+		void setViewport(const VkViewport& viewport) noexcept;
+		
+		void setScissor(const VkRect2D& scissor) noexcept;
+		
+		void draw(std::uint32_t vertexCount,
+			std::uint32_t instanceCount,
+			std::uint32_t firstVertex,
+			std::uint32_t firstInstance) noexcept;
 	};
+
+	inline bool CommandBuffer::reset() noexcept
+	{
+		const auto result = vkResetCommandBuffer(objectHandle, 0);
+		if (result == VK_SUCCESS)
+		{
+			return true;
+		}
+		else
+		{
+			Logger->error("Couldn't reset, result: {}", static_cast<std::int32_t>(result));
+			return false;
+		}
+	}
+
+	inline bool CommandBuffer::begin() noexcept
+	{
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+		beginInfo.flags = 0;
+		beginInfo.pInheritanceInfo = nullptr;
+
+		const auto result = vkBeginCommandBuffer(objectHandle, &beginInfo);
+		if (result == VK_SUCCESS)
+		{
+			return true;
+		}
+		else
+		{
+			Logger->error("Couldn't begin, result: {}", static_cast<std::int32_t>(result));
+			return false;
+		}
+	}
+
+	inline bool CommandBuffer::end() noexcept
+	{
+		const auto result = vkEndCommandBuffer(objectHandle);
+		if (result == VK_SUCCESS)
+		{
+			return true;
+		}
+		else
+		{
+			Logger->error("Couldn't end, result: {}", static_cast<std::int32_t>(result));
+			return false;
+		}
+	}
+
+	inline void CommandBuffer::endRenderPass() noexcept
+	{
+		vkCmdEndRenderPass(objectHandle);
+	}
+
+	inline void CommandBuffer::setViewport(const VkViewport& viewport) noexcept
+	{
+		vkCmdSetViewport(objectHandle, 0, 1, &viewport);
+	}
+
+	inline void CommandBuffer::setScissor(const VkRect2D& scissor) noexcept
+	{
+		vkCmdSetScissor(objectHandle, 0, 1, &scissor);
+	}
+
+	inline void CommandBuffer::draw(std::uint32_t vertexCount, std::uint32_t instanceCount, std::uint32_t firstVertex, std::uint32_t firstInstance) noexcept
+	{
+		vkCmdDraw(objectHandle, vertexCount, instanceCount, firstVertex, firstInstance);
+	}
+
 }
--- Libs/VulkanRendererLib/source/ZtCommandBuffer.cpp
+++ Libs/VulkanRendererLib/source/ZtCommandBuffer.cpp
@@ -31,53 +31,6 @@ namespace zt::vulkan_renderer
 		}
 	}
 
-	bool CommandBuffer::reset() noexcept
-	{
-		const auto result = vkResetCommandBuffer(objectHandle, 0);
-		if (result == VK_SUCCESS)
-		{
-			return true;
-		}
-		else
-		{
-			Logger->error("Couldn't reset, result: {}", static_cast<std::int32_t>(result));
-			return false;
-		}
-	}
-
-	bool CommandBuffer::begin() noexcept
-	{
-		VkCommandBufferBeginInfo beginInfo{};
-		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-		beginInfo.flags = 0;
-		beginInfo.pInheritanceInfo = nullptr;
-
-		const auto result = vkBeginCommandBuffer(objectHandle, &beginInfo);
-		if (result == VK_SUCCESS)
-		{
-			return true;
-		}
-		else
-		{
-			Logger->error("Couldn't begin, result: {}", static_cast<std::int32_t>(result));
-			return false;
-		}
-	}
-
-	bool CommandBuffer::end() noexcept
-	{
-		const auto result = vkEndCommandBuffer(objectHandle);
-		if (result == VK_SUCCESS)
-		{
-			return true;
-		}
-		else
-		{
-			Logger->error("Couldn't end, result: {}", static_cast<std::int32_t>(result));
-			return false;
-		}
-	}
-
 	void CommandBuffer::beginRenderPass(const RenderPass& renderPass, const Framebuffer& framebuffer, const VkExtent2D& extent, const Vector2i& offset, const VkClearValue& clearValue) noexcept
 	{
 		VkRenderPassBeginInfo beginInfo{};
@@ -93,29 +46,8 @@ namespace zt::vulkan_renderer
 		vkCmdBeginRenderPass(objectHandle, &beginInfo, VK_SUBPASS_CONTENTS_INLINE);
 	}
 
-	void CommandBuffer::endRenderPass() noexcept
-	{
-		vkCmdEndRenderPass(objectHandle);
-	}
-
 	void CommandBuffer::bindPipeline(const Pipeline& pipeline) noexcept
 	{
 		vkCmdBindPipeline(objectHandle, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.get());
 	}
-
-	void CommandBuffer::setViewport(const VkViewport& viewport) noexcept
-	{
-		vkCmdSetViewport(objectHandle, 0, 1, &viewport);
-	}
-
-	void CommandBuffer::setScissor(const VkRect2D& scissor) noexcept
-	{
-		vkCmdSetScissor(objectHandle, 0, 1, &scissor);
-	}
-
-	void CommandBuffer::draw(std::uint32_t vertexCount, std::uint32_t instanceCount, std::uint32_t firstVertex, std::uint32_t firstInstance) noexcept
-	{
-		vkCmdDraw(objectHandle, vertexCount, instanceCount, firstVertex, firstInstance);
-	}
-
 }
