e84bcd6076c4f3fca364c4df0a661e7af1c95721
Kaninchen221 <pawel8krolik@gmail.com>
2025-07-10 23:28:45 +0200

    NO_TASK Remove empty Sprite class

--- Libs/VulkanRendererLib/source/ZtSprite.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zinet/VulkanRenderer/ZtSprite.hpp"
-#include "Zinet/VulkanRenderer/ZtRendererContext.hpp"
-#include "Zinet/VulkanRenderer/ZtDrawInfo.hpp"
-
-namespace zt::vulkan_renderer
-{
-
-}
