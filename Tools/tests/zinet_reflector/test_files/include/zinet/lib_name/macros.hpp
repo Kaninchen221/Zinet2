5e480960606163e9b89be3f6973a9114e5864b52
Kaninchen221 <pawel8krolik@gmail.com>
2025-07-23 19:04:55 +0200

    NO_TASK Remove reflector tool

--- Tools/tests/zinet_reflector/test_files/include/zinet/lib_name/macros.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define ZINET_ENGINE_API
-#define ZT_GENERATED_BODY(...)
-#define ZT_REFLECT_NAMESPACE(...)
-#define ZT_REFLECT_CLASS(...)
-#define ZT_REFLECT_METHOD(...)
-#define ZT_REFLECT_MEMBER(...)
-#define ZT_REFLECT_STRUCT(...)
--- Tools/zinet_reflector/__init__.py
+++
@@ -1,3 +0,0 @@
-import pathlib
-import sys
-sys.path.insert(0, pathlib.Path(".").absolute() / "/reflector")
