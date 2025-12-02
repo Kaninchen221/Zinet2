d77afd3554b4647b0ae93ee0a138fd596f8ca105
Kaninchen221 <pawel8krolik@gmail.com>
2025-12-02 23:13:17 +0100

    Add TODOs and refactor

--- Libs/GameplayLib/tests_automatic/source/Systems/ZtSystemSpritesTests.cpp
+++ Libs/GameplayLib/tests_automatic/source/Systems/ZtSystemSpritesTests.cpp
@@ -2,6 +2,7 @@
 
 #include "Zinet/Gameplay/Assets/ZtAssetShader.hpp"
 #include "Zinet/Gameplay/Assets/ZtAssetSampler.hpp"
+#include "Zinet/Gameplay/Assets/ZtAssetTexture.hpp"
 
 #include <gtest/gtest.h>
 
@@ -32,6 +33,18 @@ namespace zt::gameplay::system::tests
 		void TearDown() override
 		{
 			auto rendererRes = world.getResource<VulkanRenderer>();
+			auto& rendererContext = rendererRes->getRendererContext();
+			auto& vma = rendererContext.getVMA();
+
+			ecs::Query<Buffer> buffers{ world };
+			for (auto [buffer] : buffers)
+			{
+				if (buffer && buffer->isValid())
+				{
+					buffer->destroy(vma);
+				}
+			}
+
 			ASSERT_TRUE(rendererRes);
 			rendererRes->deinit();
 
@@ -44,13 +57,16 @@ namespace zt::gameplay::system::tests
 		ecs::Schedule schedule;
 	};
 
+	// TODO: (Sprites)
+	// - Parametrize shaders
+	// - Parametrize used texture (or add something like atlas of textures?)
 	TEST_F(SpritesTests, Test)
 	{
 		auto rendererRes = world.addResource(VulkanRenderer{});
 		ASSERT_TRUE(rendererRes->init(window));
 
 		AssetsStorage assetsStorage;
-		//assetsStorage.registerAssetClass<asset::Texture>();
+		assetsStorage.registerAssetClass<asset::Texture>();
 		assetsStorage.registerAssetClass<asset::Shader>();
 		assetsStorage.registerAssetClass<asset::Sampler>();
 		assetsStorage.storeAssets();
@@ -65,19 +81,22 @@ namespace zt::gameplay::system::tests
 		{ // Init
 			schedule.runOneSystemOnce(Sprites{}, Sprites::Init, world);
 
-			ecs::Query<Sprite, Buffer> query{ world };
+			ecs::Query<Sprite, Buffer, ConstAssetHandle<asset::Texture>> query{ world };
 
-			// We expect the label, and one buffer
-			ASSERT_EQ(query.getComponentsCount(), 2);
+			// We expect the label, one buffer and one const asset handle to the texture
+			//ASSERT_EQ(query.getComponentsCount(), 3);
 
-			for (auto [label, transformBuffer] : query)
+			for (auto [label, transformBuffer, texture] : query)
 			{
 				ASSERT_TRUE(transformBuffer->isValid());
 
 				const size_t expectedSize = spritesCount * sizeof(Transform);
 				ASSERT_EQ(expectedSize, transformBuffer->getSize());
+
+				//ASSERT_TRUE(texture);
 			}
 
+			// Texture
 			// Descriptors
 			// GraphicsPipeline
 			// RenderDrawData
@@ -88,26 +107,14 @@ namespace zt::gameplay::system::tests
 		}
 
 		{ // Update
-			schedule.addSystem(Sprites{}, Sprites::Update);
-
-			schedule.buildGraph();
-			schedule.resolveGraph();
-			schedule.runOnce(world);
-
-			schedule.clear();
+			schedule.runOneSystemOnce(Sprites{}, Sprites::Update, world);
 
 			if (auto exitReason = world.getResource<ExitReason>())
 				FAIL() << exitReason->reason;
 		}
 
 		{ // Deinit
-			schedule.addSystem(Sprites{}, Sprites::Deinit);
-
-			schedule.buildGraph();
-			schedule.resolveGraph();
-			schedule.runOnce(world);
-
-			schedule.clear();
+			schedule.runOneSystemOnce(Sprites{}, Sprites::Deinit, world);
 
 			if (auto exitReason = world.getResource<ExitReason>())
 				FAIL() << exitReason->reason;
